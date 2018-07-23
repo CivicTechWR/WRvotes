2ba33f59f99cd70f6c56387290483a113366463e
Paul Nijjar <pnijjar@alumni.uwaterloo.ca>
2018-07-23 03:21:03 -0400

    Fix class for candidate listing.

--- docs/assets/js/hide-listings.js
+++ docs/assets/js/hide-listings.js
@@ -11,7 +11,7 @@ $( document ).ready(function() {
   function get_description( id ) { 
     var desc = "Results";
 
-    if ($(id).hasClass("race-wrapper")) { 
+    if ($(id).hasClass("candidate-list")) { 
       desc = "Candidate Listing";
     } else if ($(id).hasClass("event-list")) { 
       desc = "Events";
