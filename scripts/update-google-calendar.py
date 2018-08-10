c0052f4c8faadc959a82d1fcbd1324e2a522a439
Paul Nijjar <pnijjar@alumni.uwaterloo.ca>
2018-08-10 00:44:05 -0400

    Add (non-functioning) calendar clearing, and (functional) ability to filter only on upcoming events

--- scripts/update-google-calendar.py
+++ scripts/update-google-calendar.py
@@ -9,9 +9,10 @@ Paul "Worthless" Nijjar, 2018-08-08
 import csv, json
 import argparse, sys, os
 import pprint
-import dateutil.parser
+import dateutil.parser, pytz, datetime
 from google.oauth2 import service_account
 import googleapiclient.discovery
+
 
 # '/home/pnijjar/watcamp/python_rss/gcal_helpers/config.py'
 # See: http://www.karoltomala.com/blog/?p=622
@@ -20,10 +21,25 @@ DEFAULT_CONFIG_SOURCEFILE = os.path.join(
     'update-google-calendar.config.py',
     )
 
+
+INVALID_DATE = datetime.datetime(1969,12,12,
+    tzinfo=pytz.timezone('UTC'))
+
 events_dict = {}
 positions_dict = {}
 
-script_operation = None
+# ------------------------------
+def get_today():
+    """ Gets current time as of midnight."""
+
+    target_timezone = pytz.timezone(config.TIMEZONE)
+    today = datetime.datetime.now(tz=target_timezone)
+
+    today = today.replace(hour=0, minute=0, second=0)
+
+    return today
+
+
 
 # -----------------------------
 def get_padded_id(id_str):
@@ -71,14 +87,15 @@ def load_config(configfile=None):
         )
     parser.add_argument('-o', '--operation',
         help='operation to perform',
-        default='sync_upcoming',
-        choices=['sync-upcoming','sync-all', 'clear']
+        default='sync-upcoming',
+        choices=['sync-upcoming','sync-all', 'clear-calendar']
         )
 
     args = parser.parse_args()
     if args.configfile:
         config_location = os.path.abspath(args.configfile)
 
+    global script_operation
     script_operation = args.operation
 
     # http://stackoverflow.com/questions/11990556/python-how-to-make-global 
@@ -148,78 +165,110 @@ def connect_to_calendar():
         )
     return cal_object
 
+
+# ------------------------------
+def sync_calendar(cal, include_all=False):
+    """ Synchronize the events.csv to the Google calendar.
+    """
+
+    load_csv_dicts()
+
+    since_when = get_today()
+
+    if include_all:
+        since_when = INVALID_DATE
+
+    # Format looks like: 2017-03-25T00:00:00-0500
+    since_when_formatted = since_when.strftime("%Y-%m-%dT%H:%M:%S%z")
+
+    # Grab events we know so far. 
+    existing_events = cal.events().list(
+        calendarId=config.CALENDAR_ID,
+        orderBy='updated',
+        singleEvents='true',
+        showDeleted='true',
+        timeMin = since_when_formatted,
+        ).execute()
+
+    # Map existing IDs 
+    existing_ids = {}
+    for ev in existing_events['items']:
+        existing_ids[ev['id']] = True
+
+    for k in sorted(events_dict.keys()):
+        ev = events_dict[k]
+
+        datetime_end = dateutil.parser.parse(ev['DateTimeEnd'])
+        # Make date timezone-aware (sigh)
+        tz = pytz.timezone(config.TIMEZONE)
+        datetime_end = tz.localize(datetime_end)
+
+        # If the event is in the past, then 
+        if datetime_end < since_when:
+            print("IGNORED: {}".format(ev['Title']))
+            continue
+           
+        desc = '<p>Website: <a href="{}">{}</a></p>'.format(
+            ev['URL'],
+            ev['URL'],
+            )
+        desc += '<p>{}</p><p>Tags: {}</p>'.format(
+            ev['Notes'],
+            ev['PositionIDList'],
+            )
+
+        bodydict = {
+            'start':{
+                'dateTime': get_datetime(ev['DateTimeStart']),
+                'timeZone': config.TIMEZONE,
+                },
+            'end':{
+                'dateTime': get_datetime(ev['DateTimeEnd']),
+                'timeZone': config.TIMEZONE,
+                },
+            'id': get_padded_id(ev['RowID']),
+            'description': desc,
+            'location': ev['Location'],
+            'source': { 'url' : ev['URL']},
+            'summary': ev['Title'],
+            }
+        
+        if ev['CancelledOrRescheduled'] == "Cancelled":
+            bodydict['status'] = 'cancelled'
+
+        # If the entry exists then update, else create
+        if get_padded_id(ev['RowID']) in existing_ids:
+            print("Update: {}".format(ev['Title']))
+            event_add = cal.events().update(
+                calendarId=config.CALENDAR_ID,
+                eventId=get_padded_id(ev['RowID']),
+                body=bodydict,
+                ).execute()
+        else:
+            print("Insert: {}".format(ev['Title']))
+            event_add = cal.events().insert(
+                calendarId=config.CALENDAR_ID,
+                body=bodydict,
+                ).execute()
+
+    #pprint.pprint(existing_events)
+
+
+
 # ===== MAIN PROGRAM =======
 
 load_config()
-
-load_csv_dicts()
-
 cal = connect_to_calendar()
 
-# Need to handle exceptions in responses!
-# Grab events we know so far. 
-existing_events = cal.events().list(
-    calendarId=config.CALENDAR_ID,
-    orderBy='updated',
-    showDeleted='true',).execute()
+#print("Main: script_operation: {}".format(script_operation))
 
-# Map existing IDs 
-existing_ids = {}
-for ev in existing_events['items']:
-    existing_ids[ev['id']] = True
-
-for k in sorted(events_dict.keys()):
-    ev = events_dict[k]
-
-    desc = '<p>Website: <a href="{}">{}</a></p>'.format(
-        ev['URL'],
-        ev['URL'],
-        )
-    desc += '<p>{}</p><p>Tags: {}</p>'.format(
-        ev['Notes'],
-        ev['PositionIDList'],
-        )
-
-    bodydict = {
-        'start':{
-            'dateTime': get_datetime(ev['DateTimeStart']),
-            'timeZone': config.TIMEZONE,
-            },
-        'end':{
-            'dateTime': get_datetime(ev['DateTimeEnd']),
-            'timeZone': config.TIMEZONE,
-            },
-        'id': get_padded_id(ev['RowID']),
-        'description': desc,
-        'location': ev['Location'],
-        'source': { 'url' : ev['URL']},
-        'summary': ev['Title'],
-        }
-    
-    if ev['CancelledOrRescheduled'] == "Cancelled":
-        bodydict['status'] = 'cancelled'
-
-    # If the entry exists then update, else create
-    if get_padded_id(ev['RowID']) in existing_ids:
-        print("Update: {}".format(ev['Title']))
-        event_add = cal.events().update(
-            calendarId=config.CALENDAR_ID,
-            eventId=get_padded_id(ev['RowID']),
-            body=bodydict,
-            ).execute()
-    else:
-        print("Insert: {}".format(ev['Title']))
-        event_add = cal.events().insert(
-            calendarId=config.CALENDAR_ID,
-            body=bodydict,
-            ).execute()
-
-    #pprint.pprint(event_add)
-
-     
-    # print("{} : {}".format(
-    #    k,
-    #    events_dict[k]['Title'],
-    #    ))
-
-#pprint.pprint(existing_events)
+if script_operation == 'sync-upcoming':
+    sync_calendar(cal)
+elif script_operation == 'sync-all':
+    sync_calendar(cal, True)
+elif script_operation == 'clear-calendar':
+    # Yikes! Danger! Fortunately this does not work..
+    cal.calendars().clear(calendarId=config.CALENDAR_ID).execute()
+    print("Yikes! Calendar cleared.")
+else:
+    print("Unknown operation: {}".format(script_operation))
