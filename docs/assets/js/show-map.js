93076004f7924b0651bbc75fb51e1a612da6bac2
Paul Nijjar <pnijjar@alumni.uwaterloo.ca>
2018-08-24 04:16:52 -0400

    Try simplifying search?

--- docs/assets/js/show-map.js
+++ docs/assets/js/show-map.js
@@ -13,7 +13,6 @@ map.addLayer(
 map.addControl( new L.Control.Search({
         url: 'https://nominatim.openstreetmap.org/search?format=json&countrycodes=ca&viewbox=-80.7907,43.2281,-80.0834,43.6032&bounded=1&q={s}',
         jsonpParam: 'json_callback',
-        propertyName: 'display_name',
         propertyLoc: ['lat','lon'],
         marker: L.circleMarker([0,0],{radius:30}),
         autoCollapse: false,
@@ -21,7 +20,8 @@ map.addControl( new L.Control.Search({
         autoType: false,
         container: 'map-searchbar',
         zoom: 15,
-        tooltipLimit: 0,
         minLength: 10
 }) );
 
+//        propertyName: 'display_name',
+//        tooltipLimit: 0,
