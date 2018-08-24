a3a9727fc172e6952b848cc38fc2a219cf55f2f9
Paul Nijjar <pnijjar@alumni.uwaterloo.ca>
2018-08-24 01:44:38 -0400

    Try to fix random A?

--- docs/assets/js/show-map.js
+++ docs/assets/js/show-map.js
@@ -7,7 +7,7 @@ map.addLayer(new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 map.addControl( new L.Control.Search({
         url: 'https://nominatim.openstreetmap.org/search?format=json&countrycodes=ca&viewbox=-80.7907,43.2281,-80.0834,43.6032&bounded=1&q={s}',
         jsonpParam: 'json_callback',
-        propertyName: 'display_name',A
+        propertyName: 'display_name',
         propertyLoc: ['lat','lon'],
         marker: L.circleMarker([0,0],{radius:30}),
         autoCollapse: true,
