549b5f58d7c37190a474addd9ffe681b202e09eb
Paul Nijjar <pnijjar@alumni.uwaterloo.ca>
2018-08-24 23:10:54 -0400

    Try making popup on successful search

--- docs/assets/js/show-map.js
+++ docs/assets/js/show-map.js
@@ -23,28 +23,35 @@ function onEachFeature(feature, layer) {
 
 
 var attrib = 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
-var map = new L.Map('map', 
-  {zoom: 10, 
-  center: new L.latLng([43.418611, -80.472778]),
-  scrollWheelZoom:false,
+var map = new L.Map('map', {
+    zoom: 10, 
+    center: new L.latLng([43.418611, -80.472778]),
+    scrollWheelZoom:false,
   });
+
 map.addLayer(
   new L.TileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
     { attribution: attrib }));     //base layer
 
-map.addControl( new L.Control.Search({
+var searchControl = new L.Control.Search({
         url: 'https://nominatim.openstreetmap.org/search?format=json&countrycodes=ca&viewbox=-80.7907,43.2281,-80.0834,43.6032&bounded=1&q={s}',
         jsonpParam: 'json_callback',
         propertyLoc: ['lat','lon'],
         propertyName: 'display_name',
-        marker: L.circleMarker([0,0],{radius:30}),
+        marker: false,
         autoCollapse: false,
         collapsed: false,
         autoType: false,
         container: 'map-searchbar',
         zoom: 15,
         minLength: 3
-}) );
+    });
+
+searchControl.on('search_locationfound', function(e) { 
+    e.layer.openPopup().openOn(map);
+});
+
+map.addControl(searchControl);
 
 $.getJSON("./assets/data/WardBoundaries.geojson", function(data) {
     var geojson = L.geoJson(data, {
