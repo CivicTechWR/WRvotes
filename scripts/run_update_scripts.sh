ad2aa1c5c3227c8bd65b0d1e73f98ad7f7816aef
Paul Nijjar <pnijjar@vps03.kwlug.org>
2022-08-31 02:31:36 -0400

    Document crontab

--- scripts/run_update_scripts.sh
+++ scripts/run_update_scripts.sh
@@ -12,3 +12,7 @@ cd $2
 python3 get-csv.py --configfile $3
 python3 update-google-calendar.py --configfile $4
 
+
+# Sample crontab entry
+# Update WRV information 
+#1,16,31,46 * * * * /home/pnijjar/WRVotesMunicipal2022/scripts/run_update_scripts.sh /home/pnijjar/WRVotesMunicipal2022/scripts/venv/ /home/pnijjar/WRVotesMunicipal2022/scripts/ /home/pnijjar/WRVotesMunicipal2022/scripts/get-csv-config.yml /home/pnijjar/WRVotesMunicipal2022/scripts/update-google-calendar.config.py >> /home/pnijjar/logs/cronjob.log
