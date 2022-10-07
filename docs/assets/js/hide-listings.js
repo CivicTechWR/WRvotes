4fddf015e34514c9df8e83d24d8ae8714a5fa100
Paul Nijjar <pnijjar@alumni.uwaterloo.ca>
2022-10-06 22:57:26 -0400

    Move JS more button to bottom of list

--- docs/assets/js/hide-listings.js
+++ docs/assets/js/hide-listings.js
@@ -21,12 +21,15 @@ $( document ).ready(function() {
       desc = "News Items";
     } else if ($(id).hasClass("blogs-list")) { 
       desc = "Opinions";
+    } else if ($(id).hasClass("recordings-list")) {
+      desc = "Recordings";
     } 
 
     return desc;
   } // get_description  
 
-  function add_button_to_parent ( ) { 
+
+  function add_toggle_button ( ) { 
     var target =  $(this).attr('id');
     var classes = $(this).attr('class').split(/\s+/);
 
@@ -39,15 +42,16 @@ $( document ).ready(function() {
       } // endif 
     } // end for
 
-    retval = '<button name="foo" '
+    retval = '<button data-ul="' + target + '" '
              + 'id="' + target + '-btn" '
-             + 'class="toggle-button test'
-             + ' ' + classes_to_add 
-             + '">'
+             + 'class="toggle-button v04'
+             + ' ' + target + ' ' + classes_to_add
+             + '" '
+             + '"targetul="' + target + '">'
              + 'Init '
              + get_description( '#' + target )
              + '</button>';
-    $(this).parent().prepend(retval);
+    $(this).parent().append(retval);
   };
 
   function toggle_listing ( target ) { 
@@ -55,22 +59,25 @@ $( document ).ready(function() {
     // How the buttons should be labelled, with 
     // "More" or "Fewer" prepended.
     var div_description = get_description( target );
+    var target_ul = "#" + $(target).attr('data-ul');
 
     if ($(target).hasClass('hidden')) {
       $(target).removeClass('hidden');
       $(target).text("Show Fewer " + div_description); 
-      $(target + " ~ .togglable").show();
+      // Select the ID with the given prefix
+      $(target_ul).show();
     } else {
       $(target).addClass('hidden');
       $(target).text("Show More " + div_description);
-      $(target + " ~ .togglable").hide();
+      $(target_ul).hide();
+
     }
   }; // end toggle_listing
 
   /* ----- INIT CODE ------ */
 
   // Insert buttons everywhere!
-  $(".togglable").each( add_button_to_parent );
+  $(".togglable").each( add_toggle_button );
 
   $(".toggle-button").each( function() {
     toggle_listing ("#" + this.id );
