1ced8b507c8a2f8a35e49e280b5e11c61d2244aa
Paul Nijjar <pnijjar@alumni.uwaterloo.ca>
2025-12-30 19:16:37 -0500

    Add skunkworks .docx script

---
+++ scripts/gen_worksheets.py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python3
+
+# Generate .docx/.xlsx/.pdf versions of candidate lists
+# Paul "Worthless" Nijjar, 2025-12-30
+
+import csv
+from docx import Document
+from docx.enum.table import WD_ALIGN_VERTICAL
+import os, sys
+from datetime import datetime
+
+
+DATADIR="../docs/_data"
+TEST_POSITION='SchoolBoard-Public-English-Kitchener'
+TEST_WARD='Kitchener-Ward-09'
+OUTDIR="../docs/worksheets"
+
+# I am not happy with reading and appending into a giant list, but
+# whatever. The numbers are small enough that inefficiencies should
+# not matter. 
+
+# Pandas would probably be better but I can't use NumPy on this
+# laptop without yak-shaving. 
+
+
+# ---- HELPER FUNCTIONS
+
+# From: https://stackoverflow.com/questions/37757203/making-cells-bold-in-a-table-using-python-docx
+def make_rows_bold(*rows):
+    for row in rows:
+        for cell in row.cells:
+            for paragraph in cell.paragraphs:
+                for run in paragraph.runs:
+                    run.font.bold = True
+
+
+
+# Doesn't work?
+def make_rows_vertically_centred(*rows):
+    for row in rows:
+        for cell in row.cells:
+            cell.vertical_alignment = WD_ALIGN_VERTICAL.CENTER
+
+
+
+# --- READ DATA 
+
+
+position_names = {}
+position_candidates = {}
+position_num_to_elect = {}
+position_ward = {} # ugh
+position_acclaimed = {}
+
+with open (os.path.join(DATADIR,"internal/position-tags.csv")) as r:
+    pos_csv = csv.DictReader(r)
+
+    for row in pos_csv:
+        position_names[ row['PositionUniqueName']] = row['PositionDesc']
+        position_candidates[row['PositionUniqueName']] = []
+        position_num_to_elect[row['PositionUniqueName']] = row['NumberToElect']
+        position_ward[row['PositionUniqueName']] = row['WardMunicipality']
+
+
+with open(os.path.join(DATADIR,"sync/nominees.csv")) as r:
+    nom_csv = csv.DictReader(r)
+
+    for row in nom_csv:
+        position_candidates[row['PositionUniqueName']].append(row)
+
+municipality_map = []
+with open (os.path.join(DATADIR,"internal/municipality-map.csv")) as r:
+    mun_csv = csv.DictReader(r)
+
+    for row in mun_csv:
+        municipality_map.append(row)
+
+
+# Need to sort nominees by last name? 
+for pos in position_candidates:
+    position_candidates[pos].sort(
+      key = lambda x: "{},{}".format(x['Last_Name'],x['Given_Names'])
+      )
+
+    num_candidates = len(position_candidates[pos])
+    num_seats = int(position_num_to_elect[pos])
+
+    position_acclaimed[pos] = (num_candidates <= num_seats)
+
+    
+
+# ---- GEN DOCX
+
+
+for pos,title in position_names.items():
+    if position_ward[pos] == 'N/A':
+        continue
+
+    ward = pos
+    ward_municipality = position_ward[pos]
+
+    races = "NOT-FOUND"
+
+    for m in municipality_map:
+        if m['Name'] == ward_municipality:
+            races = m['Races']
+            break
+      
+    if races == 'NOT-FOUND':
+        raise NameError
+
+    d = Document()
+
+    d.add_heading("Municipal Election Candidate Sheet", 0)
+    d.add_paragraph("List generated on {}".format(
+        datetime.now().strftime("%A, %B %e, %Y, %l:%M%P")
+        ))
+    d.add_paragraph("Use this handy sheet to figure out the candidates"
+       " you want to vote for. "
+       )
+
+    race_list = races.split(",")
+
+    for r in race_list:
+        if r == '_SELF':
+            r = ward
+
+        d.add_heading("{}".format( position_names[r]))
+
+        elected_text = "{} to be elected".format(
+            position_num_to_elect[r]
+            )
+        if position_acclaimed[r]:
+            elected_text = "{} : ACCLAIMED".format(elected_text)
+
+        d.add_paragraph(elected_text)
+
+        t = d.add_table(rows=1, cols=2)
+        header_cells = t.rows[0].cells
+        header_cells[0].text = "Candidate Name"
+        header_cells[1].text = "Notes"
+        make_rows_vertically_centred(t.rows[0])
+        make_rows_bold(t.rows[0])
+
+        for nom in position_candidates[r]:
+            row = t.add_row()
+            row_cells = row.cells
+            row_cells[0].text = "{} {}".format(
+                nom['Given_Names'],
+                nom['Last_Name'],
+                )
+            row_cells[1].text = ""
+            make_rows_vertically_centred(row)
+
+
+    d.save(os.path.join(OUTDIR,"docx","{}.docx".format(ward)))
+
+
+
+
+    
+
+
+
+# ---- BAD TESTING
+
+"""
+print(position_candidates[TEST_POSITION])
+print(len(position_candidates[TEST_POSITION'))
+
+for nom in position_candidates[TEST_POSITION]:
+    print("{} {}".format(nom['Given_Names'],nom['Last_Name']))
+
+"""
+
+
