819f3b8876715e23229345c519007f0bbf334354
Paul Nijjar <pnijjar@alumni.uwaterloo.ca>
2026-02-05 21:45:43 -0500

    Use emptyish include files instead of symlinks, so code works in Windows git

--- scripts/set-alias-includes.sh
+++ scripts/set-alias-includes.sh
@@ -1,21 +1,28 @@
 #!/bin/bash 
 
-# Given a file of aliases and  a path to a template file, create a
-# symlink of each alias to the template in the CURRENT folder.
+# Given a file of aliases and  a path to a template file, copy the
+# template of each alias to the the template in the CURRENT folder. 
+#
+# The template should probably contain an include to a centralized
+# template. (eg use "include-positions.html", not
+# "position-template.html")
 
 # Paul "Worthless" Nijjar, 2022-08-16
 
 # (This does not work well for the ward listing. The following may be
 # helpful: 
 # for i in `/bin/ls *.html | grep -v "Ward"`; do echo $i; rm $i; done
+# 
+# FIRST run this script using position-tags and the ward template.
+# THEN run the loop above to include only "Ward" listings. 
 # )
 
 usage() {
   echo $0 [alias-file] [template-file]
   echo ""
 
-  echo Create symlinks for each alias in the alias-file to the
-  echo template file in the CURRENT folder.
+  echo Copy template-file for each alias in the alias-file to the
+  echo CURRENT folder.
 
 }
 
@@ -54,5 +61,5 @@ aliases=$(tail +2 $alias_file | cut -f1 -d, )
 
 for a in $aliases 
 do
-  ln -sf $template ${a}.html
+  cp $template ${a}.html
 done
