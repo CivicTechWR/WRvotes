d70d9478fb7b2d20e296870a5912e421b74e64e6
Paul Nijjar <pnijjar@alumni.uwaterloo.ca>
2026-02-26 06:15:36 -0500

    Allow accordion to work on distant siblings

--- docs/assets/js/accordion.js
+++ docs/assets/js/accordion.js
@@ -5,7 +5,19 @@
 
 function toggleAccordion(){
     this.classList.toggle('active');
-    this.nextElementSibling.classList.toggle('active');
+
+    // Sigh. Maybe I want some elements in between.
+    curr  = this.nextElementSibling;
+    while (curr) { 
+      if (curr.classList.contains("toggle-content")) { 
+         target = curr;
+         break;
+      } 
+      curr = curr.nextElementSibling;
+
+    } // end while
+
+    target.classList.toggle('active');
 }
 
 const toggles = document.querySelectorAll(".toggleable");
