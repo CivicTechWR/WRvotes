5492931684802a44060cdfac8bd98b04969aed14
Ryan Bianchi <1435081+qedi-r@users.noreply.github.com>
2026-03-22 12:13:47 -0400

    fix scaling for multi-race-votes

--- docs/_plugins/nominees_results_generator.rb
+++ docs/_plugins/nominees_results_generator.rb
@@ -12,8 +12,25 @@ module WRVotes
         return (v.nil? || v.to_s.strip.empty?) ? v_default : v.to_i
     end
 
+    def determine_race_scale(group, number_to_elect)
+      total_votes = 0
+      max_votes = 0
+      group.each do |nominee|
+        votes = votes_to_i(nominee['Votes'], 0)
+        total_votes += votes
+        max_votes = [max_votes, votes].max()
+      end
+
+      if number_to_elect > 1
+        return max_votes
+      else
+        return total_votes
+      end
+    end
+
     def generate(site)
       nominees  = site.data.dig('sync', 'nominees')
+
 
       unless nominees 
         Jekyll.logger.warn 'NomineesResultsGenerator:', \
@@ -26,13 +43,14 @@ module WRVotes
       sorted_nominees_results = {}
       grouped_by_race.each do |race_id, group|
         next if race_id.nil? || race_id.empty?
-        max_votes = 0
-        group.each do |nominee|
-          max_votes += votes_to_i(nominee['Votes'], 0)
+
+        race = site.data.dig('internal','position-tags').find do |r|
+          r['PositionUniqueName'] == race_id
         end
 
+        vote_scale = determine_race_scale(group, race['NumberToElect'].to_i)
         group.each do |nominee|
-          nominee['Max_votes'] = max_votes
+          nominee['Vote_scale'] = vote_scale
         end
 
         group = group.filter do |nominee|
