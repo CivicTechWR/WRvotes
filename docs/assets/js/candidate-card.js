a147fd723da1d2b5df47a227e092b6fd03d534b7
Ryan Bianchi <1435081+qedi-r@users.noreply.github.com>
2026-04-13 22:07:12 -0400

    integrate the worksheet and the candidate cards

--- docs/assets/js/candidate-card.js
+++ docs/assets/js/candidate-card.js
@@ -1,4 +1,47 @@
+function parseFavourites() {
+    var favouritesRaw = localStorage.getItem("wrv-favourited");
+    return favouritesRaw ? JSON.parse(favouritesRaw) : [];
+}
+
+function saveFavourite(candidate) {
+    var favourites = parseFavourites();
+    if (!favourites.includes(candidate)) {
+        favourites.push(candidate);
+        localStorage.setItem('wrv-favourited', JSON.stringify(favourites));
+    }
+}
+
+function removeFavourite(candidate) {
+    var favourites = parseFavourites();
+    favourites = favourites.filter(x => x != candidate);
+    localStorage.setItem('wrv-favourited', JSON.stringify(favourites));
+}
+
+function saveNote(candidate, content) {
+    localStorage.setItem('wrv-note-' + candidate, content);
+}
+
+function loadNote(candidate) {
+    return localStorage.getItem('wrv-note-' + candidate) || '';
+}
+
 $(document).ready(function () {
+    $(".auto-resize-textarea[data-candidate-id]").each(function () {
+        var saved = loadNote($(this).attr("data-candidate-id"));
+        if (saved) {
+            $(this).html(saved);
+        }
+    });
+
+    var debounce;
+    $(".auto-resize-textarea[data-candidate-id]").on("input", function () {
+        var el = $(this);
+        clearTimeout(debounce);
+        debounce = setTimeout(function () {
+            saveNote(el.attr("data-candidate-id"), el.html());
+        }, 300);
+    });
+
     $("h3.no-website").on("click", (e) => {
         tooltip_id = $(e.target).attr('aria-describedby');
         tooltip_e = $("#" + tooltip_id)
@@ -9,4 +52,25 @@ $(document).ready(function () {
             }, 3000);
         }
     });
+
+    $(".favourite-btn").on("click", function () {
+        var candidate = $(this).attr("data-candidate-id");
+        $(this).toggleClass("favourited");
+        var icon = $(this).find("i");
+        if ($(this).hasClass("favourited")) {
+            icon.removeClass("fa-regular").addClass("fa-solid");
+            saveFavourite(candidate);
+        } else {
+            icon.removeClass("fa-solid").addClass("fa-regular");
+            removeFavourite(candidate);
+        }
+    });
+
+    for (var candidate of parseFavourites()) {
+        var btn = $(".favourite-btn[data-candidate-id='" + candidate + "']");
+        if (btn.length) {
+            btn.addClass("favourited");
+            btn.find("i").removeClass("fa-regular").addClass("fa-solid");
+        }
+    }
 });
