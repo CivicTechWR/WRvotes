be1534b2abef23e61a448491016cc955584ef24a
Ryan Bianchi <1435081+qedi-r@users.noreply.github.com>
2026-04-15 19:33:39 -0400

    add a note button, and use it in the worksheets button to try and link the concepts

--- docs/assets/js/candidate-card.js
+++ docs/assets/js/candidate-card.js
@@ -85,6 +85,22 @@ $(document).ready(function () {
         }
     });
 
+    $(".notes-btn").on("click", function () {
+        var candidate = $(this).attr("data-candidate-id");
+        $(this).toggleClass("opened");
+        var icon = $(this).find("i");
+        var notes = $(`.auto-resize-textarea[data-candidate-id='${candidate}']`);
+        if (notes) { 
+            if ($(this).hasClass("opened")) {
+                icon.removeClass("fa-regular").addClass("fa-solid");
+                notes.removeClass("hidden");
+            } else {
+                icon.removeClass("fa-solid").addClass("fa-regular");
+                notes.addClass("hidden");
+            }
+        }
+    });
+
     updateNotes();
     updateFavourites();
 
